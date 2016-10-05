1268cf70b3923894894b772e87d66fcb8f2a9b5a
sjorsduffhues <g.s.duffhues@student.tue.nl>
2016-10-05 13:35:37 +0900

    plot1

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 data<-read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
 subdata<-data[data$Date=="1/2/2007"|data$Date=="2/2/2007",]
 
-#select data and construct histogram
+#select data and construct hist
 png("plot1.png", width=480, height=480)
 hist(as.numeric(subdata$Global_active_power), col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
 dev.off()
